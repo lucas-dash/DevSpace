ef752e42f4784fb52e7d8598360bd8d69b9336cd
lucas-dash <homolka.lukas10@gmail.com>
2023-12-10 15:03:31 +0100

    initial setup, theme, colors

---
+++ src/components/HomeNav.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+import { ModeToggle } from './ui/mode-toggle';
+import Link from 'next/link';
+
+export default function HomeNav() {
+  return (
+    <nav className="container h-14 flex items-center justify-between">
+      <Link
+        href={'/'}
+        aria-label="back to homepage"
+        className="flex items-center gap-3"
+      >
+        <Image src="/logo.svg" alt="DevSpace" width={40} height={40} />
+        <h3 className="font-bold text-lg text-secondary dark:text-secondary-dark">
+          DevSpace
+        </h3>
+      </Link>
+
+      <ModeToggle />
+    </nav>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,46 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ['class'],
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
   ],
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      keyframes: {
+        'accordion-down': {
+          from: { height: 0 },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
+      colors: {
+        primary: '#FFFCF9',
+        'primary-dark': '#070707',
+        secondary: '#070707',
+        'secondary-dark': '#FFFCF9',
+        background: '#E9E9E9',
+        'background-dark': '#171717',
+        accent: '#6328E2',
+        'accent-dark': '#8B58F8',
       },
     },
   },
-  plugins: [],
-}
-export default config
+  plugins: [require('tailwindcss-animate')],
+};
