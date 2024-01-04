b9dd7e1356c6e2fede1657ff6dec62e4ec45f293
lucas-dash <homolka.lukas10@gmail.com>
2024-01-04 19:38:09 +0100

    postId style

--- src/app/(main)/home/[postId]/page.tsx
+++ src/app/(main)/home/[postId]/page.tsx
@@ -1,6 +1,8 @@
 import Post from '@/components/post/post';
+import { Button } from '@/components/ui/button';
 import { createSupabaseServerClient } from '@/lib/supabase/server';
 import { cookies } from 'next/headers';
+import Link from 'next/link';
 
 type PostType = {
   params: {
@@ -11,6 +13,11 @@ type PostType = {
 export default async function PostId({ params: { postId } }: PostType) {
   const cookieStore = cookies();
   const supabase = createSupabaseServerClient(cookieStore);
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
   const { data: post } = await supabase
     .from('posts')
     .select('*')
@@ -18,11 +25,21 @@ export default async function PostId({ params: { postId } }: PostType) {
     .single();
 
   if (!post) {
-    return;
+    return (
+      <div className="h-full flex flex-col gap-2 items-center justify-center">
+        <h3 className="font-medium text-lg">Post doesn&apos;t exists.</h3>
+        <Button asChild variant={'accent'}>
+          <Link href={'/home'}>Back to home</Link>
+        </Button>
+      </div>
+    );
   }
   return (
-    <section>
+    <section className="h-full flex flex-col gap-5 bg-primary dark:bg-primary-dark rounded-2xl">
       <Post {...post} />
+      <hr />
+      {/* comment form */}
+      <hr />
       {/* //todo comments */}
     </section>
   );
